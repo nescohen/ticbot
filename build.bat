874676fdcc1d3f6e43b984b99890dba00ff6aece
Nes Cohen <gomariners1129@gmail.com>
2016-05-23 17:45:18 -0700

    create src and bin directory, move source files

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-gcc -std=c99 -g -Wall -O2 -static -o bot tictac.c
+gcc -std=c99 -g -Wall -O2 -static -o bin\bot src\tictac.c
 rm -f tictac.zip
-7z a -tzip tictac.zip tictac.c > nul
+7z a -tzip tictac.zip src\tictac.c > nul
