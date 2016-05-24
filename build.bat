8b3a574d02d37cba993f7cac399195951ef65b87
Nes Cohen <gomariners1129@gmail.com>
2016-05-24 13:13:27 -0700

    yay, it works

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 gcc -std=c99 -g -Wall -O2 -static -o bin\bot src\board.c src\debug.c src\globals.c src\in_out.c src\main.c src\memory.c src\tree.c
 rm -f tictac.zip
 pushd src
-7z a -tzip ..\tictac.zip board.c debug.c globals.c in_out.c main.c memory.c tree.c > nul
+7z a -tzip ..\tictac.zip board.c debug.c globals.c in_out.c main.c memory.c tree.c *.h> nul
 popd
